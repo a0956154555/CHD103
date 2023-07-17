79ada9a8d5e1197cc5cc6434726c735700b66caa
a0956154555 <wayne870726@yahoo.com.tw>
2023-07-17 21:27:10 +0800

    news firstPart

--- furniture/js/news2.js
+++ furniture/js/news2.js
@@ -10,3 +10,44 @@ topCase.addEventListener("click", function () {
     underCase.style.display = "none";
   }
 });
+
+let newsTitle = document.querySelector(".news-title");
+let newsTitleArr = [
+  "N",
+  "E",
+  "V",
+  '<span class="news-title-span">V</span>',
+  "S",
+];
+let newsTitleNum = 0;
+let newsTitleInterval = setInterval(() => {
+  if (newsTitleNum < newsTitleArr.length) {
+    newsTitle.innerHTML += newsTitleArr[newsTitleNum];
+    newsTitleNum++;
+  } else {
+    clearInterval(newsTitleInterval);
+  }
+}, 300);
+let newsTitleSpan = document.querySelector(".news-title-span");
+let mainControlWidthTitle = document.querySelector(".main-control-width-title");
+
+let lastScrollPosition = document.documentElement.scrollTop;
+window.addEventListener("scroll", function () {
+  let currentScrollPosition = document.documentElement.scrollTop;
+
+  if (currentScrollPosition > lastScrollPosition) {
+    newsTitle.style.opacity = "0";
+    setTimeout(() => {
+      newsTitle.style.opacity = "1";
+    }, 1000);
+    newsTitle.style.backgroundImage = "url(../../news2-img/2.jpg)";
+  } else {
+    newsTitle.style.opacity = "0";
+    setTimeout(() => {
+      newsTitle.style.opacity = "1";
+    }, 1000);
+    newsTitle.style.backgroundImage = "url(../../news2-img/1.jpg)";
+  }
+
+  lastScrollPosition = currentScrollPosition;
+});
