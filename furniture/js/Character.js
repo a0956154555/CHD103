cd47574d05c03e52f6a3f67ec16b56b98e9cb1bd
a0956154555 <wayne870726@yahoo.com.tw>
2023-07-27 09:04:50 +0800

    updateIndex

--- furniture/js/Character.js
+++ furniture/js/Character.js
@@ -262,3 +262,17 @@ setInterval(() => {
   countDesignersTxt = 0;
   designersTxt.innerHTML = "<span>The </span>";
 }, 4500);
+
+// --------------------------手機板
+let headerMiddle = document.querySelector(".header-middle");
+let headerRight = document.querySelector(".header-right");
+let menuTrue = false;
+headerMiddle.addEventListener("click", function () {
+  menuTrue = !menuTrue;
+  if (menuTrue) {
+    headerRight.style.display = "flex";
+    headerRight.style.animation = "menuTranslate 1s forwards";
+  } else {
+    headerRight.style.display = "none";
+  }
+});
